7398e4503056ae29e4082a414cd6e65631219d47
lukekeum <lukekeum11@gmail.com>
2021-01-28 19:41:29 +0900

    Add userInfo command

---
+++ src/commands/userInfo.ts
@@ -0,0 +1,55 @@
+import { Message, MessageEmbed, User } from 'discord.js';
+import { format } from 'path';
+import bot from '../client';
+import Command, { Execute, TArguments } from '../lib/commandManager';
+
+@Command(['정보'])
+export default class Information {
+  constructor() {
+    this.execute = this.execute.bind(this);
+  }
+
+  @Execute
+  public execute(message: Message, args: TArguments) {
+    let user: User;
+    if ((args as Array<string>).length > 0 && args) {
+      user = this.getUserFromMention(args[0]);
+    } else {
+      user = message.author;
+    }
+
+    message.channel.send(this.genEmbed(user));
+  }
+
+  public genEmbed(user: User): MessageEmbed {
+    const { username, tag, createdAt } = user;
+
+    const date = `${createdAt.getFullYear()}년 ${
+      createdAt.getMonth() + 1
+    }월 ${createdAt.getDate()}일 ${createdAt.getHours()}시 ${createdAt.getMinutes()}분`;
+
+    const embed = new MessageEmbed()
+      .setTitle(`${username}님의 정보`)
+      .setThumbnail(`${user.displayAvatarURL({ dynamic: true })}`)
+      .addField('디스코드 태그', tag, false)
+      .addField('탄생일', date, false);
+
+    return embed;
+  }
+
+  private getUserFromMention(mention: string): User {
+    if (!mention.startsWith('<@') || !mention.endsWith('>'))
+      throw new Error('Mention not found');
+    mention = mention.slice(2, -1);
+
+    if (mention.startsWith('!')) {
+      mention = mention.slice(1);
+    }
+
+    const user = bot.client.users.cache.get(mention);
+
+    if (!user) throw new Error('User not found');
+
+    return user;
+  }
+}
