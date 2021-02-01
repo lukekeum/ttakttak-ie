0927a9cf45861b4221685c14cddd8387cf41015a
lukekeum <lukekeum11@gmail.com>
2021-02-01 11:45:31 +0900

    Setup express server and notice router

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import './config/env';
 import bot from './client';
 import Database from './config/database';
+import App from './app';
 
 // login Discord bot
 bot.login();
+
+// Listen express server in port 3000
+const app = new App();
+app.listen(() =>
+  console.log(`Server is running on port ${process.env.PORT || 5000}`)
+);
 
 // Connect to database
 const database = new Database();
