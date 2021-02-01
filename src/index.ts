9b1494c573c4b03b9994fbf1ca80bfd08f049766
lukekeum <lukekeum11@gmail.com>
2021-02-01 11:51:41 +0900

    Remove express with types

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,9 @@
 import './config/env';
 import bot from './client';
 import Database from './config/database';
-import App from './app';
 
 // login Discord bot
 bot.login();
-
-// Listen express server in port 3000
-const app = new App();
-app.listen(() =>
-  console.log(`Server is running on port ${process.env.PORT || 5000}`)
-);
 
 // Connect to database
 const database = new Database();
