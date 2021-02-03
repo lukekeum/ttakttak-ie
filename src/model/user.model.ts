74b34e4f4906ed83b8779d289e7342ad10af041c
lukekeum <lukekeum11@gmail.com>
2021-02-03 16:31:07 +0900

    Create User data when user message something

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -2,10 +2,12 @@ import mongoose, { Schema, Document } from 'mongoose';
 
 export interface IUser extends Document {
   id: string;
+  developer: boolean;
 }
 
 const UserSchema: Schema = new Schema({
   id: { type: String, required: true, unique: true },
+  developer: { type: Boolean, default: false },
 });
 
 export default mongoose.model<IUser>('users', UserSchema);
