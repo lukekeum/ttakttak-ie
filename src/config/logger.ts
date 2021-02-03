ff3f824c7d7c283038d6dd812cd6a5a320afdca8
lukekeum <lukekeum11@gmail.com>
2021-02-04 00:23:03 +0900

    Setup winston log files

---
+++ src/config/logger.ts
@@ -0,0 +1,35 @@
+import winston from 'winston';
+
+const { combine, timestamp, printf } = winston.format;
+
+// Define log format
+const logFormat = printf((info) => {
+  return `${info.timestamp} ${info.level}: ${info.message}`;
+});
+
+/*
+ * Log Level
+ * error: 0, warn: 1, info: 2, http: 3, verbose: 4, debug: 5, silly: 6
+ */
+const logger = winston.createLogger({
+  format: combine(
+    timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    logFormat
+  ),
+  transports: [],
+});
+
+if (process.env.NODE_ENV === 'development') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    })
+  );
+}
+
+export default logger;
