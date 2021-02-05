f64e86a3ee766752abe249693d4f01c6997f9621
lukekeum <lukekeum11@gmail.com>
2021-02-05 10:14:34 +0900

    Add guildDelete event

---
+++ src/events/guildDelete.ts
@@ -0,0 +1,22 @@
+import { Guild } from 'discord.js';
+import logger from '../config/logger';
+import Event, { Execute } from '../lib/eventManager';
+import guildModel, { IGuild } from '../model/guild.model';
+
+@Event('guildDelete')
+export default class GuildDelete {
+  @Execute
+  public async execute(guild: Guild) {
+    try {
+      const targetGuild: IGuild = guildModel.find({ id: guild.id });
+
+      if (!targetGuild) return;
+
+      await targetGuild.deleteOne();
+
+      logger.info(`${guild.name}(${guild.id}) data removed`);
+    } catch (err) {
+      logger.error(err);
+    }
+  }
+}
