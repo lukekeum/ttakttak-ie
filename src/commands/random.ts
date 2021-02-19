e7276ff74f91f5de8cf480850b4de701408964ab
lukekeum <lukekeum11@gmail.com>
2021-02-20 02:42:33 +0900

    Add random command

---
+++ src/commands/random.ts
@@ -0,0 +1,67 @@
+import Command, { Execute, TArguments } from '../lib/commandManager';
+import { Message } from 'discord.js';
+import userModel, { IUser } from '../model/user.model';
+import CreateUser from '../events/createUser';
+import logger from '../config/logger';
+
+@Command(['주사위'])
+export default class Random {
+  constructor() {
+    this.execute = this.execute.bind(this);
+  }
+
+  @Execute
+  public async execute(message: Message, args: TArguments) {
+    if (!args || args.length !== 1) {
+      return message.channel.send(
+        '들어갈 값을 입력하시지 않으신거 같아요! `명령어: 주사위 {숫자(1~6)}`'
+      );
+    }
+
+    const num = Number(args[0]);
+
+    if (isNaN(num) || num < 1 || num > 6) {
+      return message.channel.send(
+        '올바른 숫자를 입력해주세요 `명령어: 주사위 {숫자(1~6)}`'
+      );
+    }
+
+    const rolledNumber = this.randomNumber(6);
+
+    try {
+      const user: IUser = await userModel.findOne({ id: message.author.id });
+
+      if (!user) {
+        new CreateUser().execute(message);
+      }
+
+      message.channel.send(
+        `음 주사위가 굴려졌네요!\r\n주사위에서 ${rolledNumber} (이)가 나왔네요!`
+      );
+
+      if (rolledNumber === num) {
+        user.point += 8;
+
+        message.channel.send(
+          `축하드려요! 8점을 획득하셨네요${message.author.username}님!`
+        );
+      } else {
+        const reduceAmount = this.randomNumber(5);
+
+        user.point -= reduceAmount;
+
+        message.channel.send(
+          `${reduceAmount}만큼의 점수가 차감되었어요! 하지만 아직 기회는 많으니 한 번 다시 해보자구요 <@!${message.author.id}>`
+        );
+      }
+
+      await user.save();
+    } catch (err) {
+      logger.error(err);
+    }
+  }
+
+  private randomNumber(size: number) {
+    return Math.floor(Math.random() * size) + 1;
+  }
+}
--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -3,11 +3,13 @@ import mongoose, { Schema, Document } from 'mongoose';
 export interface IUser extends Document {
   id: string;
   developer: boolean;
+  point: number;
 }
 
 const UserSchema: Schema = new Schema({
   id: { type: String, required: true, unique: true },
   developer: { type: Boolean, default: false },
+  point: { type: Number, default: 0 },
 });
 
 export default mongoose.model<IUser>('users', UserSchema);
